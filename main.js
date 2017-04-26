9e18ccb03c2d16dad6b124c43bd4b3a58fc86734
dentednerd <jo.imlay@gmail.com>
2017-04-26 11:40:35 +0100

    Added jQuery to change shape of nav on mouseover and mouseout

--- main.js
+++ main.js
@@ -1,35 +1,8 @@
-// Find all YouTube videos
-var $allVideos = $("iframe[src^='//www.youtube.com']"),
-
-    // The element that is fluid width
-    $fluidEl = $("body");
-
-// Figure out and save aspect ratio for each video
-$allVideos.each(function() {
-
-  $(this)
-    .data('aspectRatio', this.height / this.width)
-
-    // and remove the hard coded width/height
-    .removeAttr('height')
-    .removeAttr('width');
-
+$(document).ready(function() {
+    $("nav").mouseover(function() {
+          $(this).css("border-radius", "10px"); 
+    });
+        $("nav").mouseout(function() {
+          $(this).css("border-radius", "50%"); 
+    });
 });
-
-// When the window is resized
-$(window).resize(function() {
-
-  var newWidth = $fluidEl.width();
-
-  // Resize all videos according to their own aspect ratio
-  $allVideos.each(function() {
-
-    var $el = $(this);
-    $el
-      .width(newWidth)
-      .height(newWidth * $el.data('aspectRatio'));
-
-  });
-
-// Kick off one resize to fix all videos on page load
-}).resize();
